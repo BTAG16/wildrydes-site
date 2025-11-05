36be1702abefd59eff3b6caaf8859bba7046601a
Cosmos Junior <rumeighoraye@gmail.com>
2025-11-05 13:11:12 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_n6rLgCP0y', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '66dp1rvmttofo8874tfmuqq9mr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_9U6RBkdKn', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6j4tm6kjib5ab9a4dn5i8tmbq6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
