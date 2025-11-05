8e084711e56557bf29c82df8952781e81b8e8b14
Cosmos Junior <rumeighoraye@gmail.com>
2025-11-05 12:22:01 +0100

    Update AWS Cognito configuration in config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_HWrs77IL6', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5hv4cjkm6ap3cq672et2sc0sdo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
